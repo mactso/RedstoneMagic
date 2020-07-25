ae8c5c9e0e25ae58699bedec01d66e22052638b5
MacTso <9662494+mactso@users.noreply.github.com>
2020-07-25 03:29:37 -0500

    adding redstone magical power with fortune breaking redstone blocks

--- src/main/java/com/mactso/redstonemagic/events/MyBreakEvent.java
+++ src/main/java/com/mactso/redstonemagic/events/MyBreakEvent.java
@@ -2,42 +2,41 @@ package com.mactso.redstonemagic.events;
 
 import com.mactso.redstonemagic.magic.CapabilityMagic;
 import com.mactso.redstonemagic.magic.IMagicStorage;
-import com.mojang.datafixers.types.templates.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
-import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class MyBreakEvent {
 
     @SubscribeEvent
-    public void onBlockBreak(HarvestDropsEvent event)
+    public void onBlockBreak(BreakEvent event)
     {
     	BlockState state = event.getState();
     	Block block = state.getBlock();
-    	if (block == Blocks.REDSTONE_ORE)
+    	if (block == Blocks.REDSTONE_ORE && event.getExpToDrop() > 0)
     	{
+    		int bonusLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, event.getPlayer().getHeldItemMainhand());
+
     		BlockPos pos = event.getPos();
     		IWorld world = event.getWorld();
     		IChunk ichunk = world.getChunk(pos);
-    		java.util.List<ItemStack> list = event.getDrops();
-    		int redstoneMagicAmount = 0;
-    		for (ItemStack stack:list) {
-    			if (stack.getItem()==Items.REDSTONE) {
-                    redstoneMagicAmount += stack.getCount(); 				
-    			}
-    		}
+//    		java.util.List<ItemStack> list = event.getDrops();
+//    		int redstoneMagicAmount = 0;
+//    		for (ItemStack stack:list) {
+//    			if (stack.getItem()==Items.REDSTONE) {
+//                    redstoneMagicAmount += stack.getCount(); 				
+//    			}
+//    		}
     		if (ichunk instanceof Chunk)
     		{
     			Chunk chunk = (Chunk) ichunk;
@@ -45,7 +44,11 @@ public class MyBreakEvent {
     			if (opt.isPresent())
     			{
     				IMagicStorage cap = opt.orElseGet(null);
-    				cap.addMagic(redstoneMagicAmount);
+    				double randint = world.getRandom().nextDouble();
+    				randint *= 3.0;
+    				int redstoneMagicIncrease = 3+(int)randint+bonusLevel;
+    				cap.addMagic(redstoneMagicIncrease);
+    				System.out.println("Increase Redstone Magic by " + redstoneMagicIncrease + " to " + cap.getMagicStored() + ".");
     			}
     		}
     	}
